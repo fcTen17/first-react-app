8fa7b71f87c654991657a173d8f3de68f49fd77e
Fernando Carvalho <fernandocesartenorio@gmail.com>
2021-10-08 15:30:42 -0300

    Files from the first-react-app exercise

--- src/App.js
+++ src/App.js
@@ -1,5 +1,84 @@
+
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+const Home = () => {
+  return <h2>Home</h2>;
+}
+const About = () => {
+  return <h2>About</h2>;
+}
+const Contact = () => {
+  return <h2>Contact</h2>;
+}
+
+const NotFound = () => {
+  return <h2>404 Not Found</h2>;
+}
+
+const App = () => {
+  return (
+    <Router>
+      <div className="container">
+        <h2>React Router Demo</h2>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <a href="/about/">About (normal anchor link, page will full reload)</a>
+            </li>
+            <li>
+              <Link to="/contact/">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" exact component={Home} />
+        <Route path="/about/" component={About} />
+        <Route path="/contact/" component={Contact} />
+        <hr />
+        <p>Using Switch</p>
+        <Switch>
+         
+          <Route path="/about/" component={About} />
+          <Route path="/contact/" component={Contact} />
+          <Route path="/" exact component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+
+export default App;
+
+
+
+
+
+
+
+
+//Original content
+
+/*
+
 import logo from './logo.svg';
 import './App.css';
+
+//import * as math from './math.js';
+//import product from './math.js';
+import product, { numbers } from './math.js';
+
+console.log(numbers.reduce(product));
+console.log(product(10, 5));
+//console.log(math.numbers.reduce(math.sum));
 
 function App() {
   return (
@@ -22,4 +101,4 @@ function App() {
   );
 }
 
-export default App;
+*/
